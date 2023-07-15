a4c0079a555a61b279d9c880796fe03945e0dd1b
SumitB1412 <beniwalsumit1412@gmail.com>
2023-07-15 12:09:20 +0530

    sample backemd deploy

--- index.js
+++ index.js
@@ -3,30 +3,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
 const connection = mongoose.connect(process.env.MONGODB_URL);
-
-const userSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  password: String,
-});
-
-const UserModel = mongoose.model("user", userSchema);
 
 app.get("/", (req, res) => {
   res.send("Homepage");
-});
-
-app.get("/users", async (req, res) => {
-  const users = await UserModel.find();
-  res.send(users);
-});
-
-app.post("/users/add", async (req, res) => {
-  const user = await UserModel(req.body);
-  await user.save();
-  res.send("User saved");
 });
 
 const PORT = process.env.PORT || 8080;
